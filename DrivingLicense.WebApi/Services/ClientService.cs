bf51627808041e2068901f1341e748fd208a4ba9
Tornike <gomarelidevelopment@gmail.com>
2020-11-26 19:58:26 +0400

    added client service

---
+++ DrivingLicense.WebApi/Services/ClientService.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+using DrivingLicense.WebApi.Models;
+using DrivingLicense.WebApi.Services.Interfaces;
+using DrivingLicense.WebApi.Infastructure.Interfaces;
+using System.Threading.Tasks;
+
+namespace DrivingLicense.WebApi.Services
+{
+    public class ClientService : IClientService
+    {
+        IRepository<LicenseCategory> _repository;
+        
+        public ClientService(IRepository<LicenseCategory> repository)
+        {
+            _repository = repository;   
+        }
+        public async void AddTicketAsync(LicenseCategory licenseCategory)
+        {
+            await Task.Run(() =>
+            {
+                if (licenseCategory != null)
+                {
+                    _repository.Insert(licenseCategory);
+                }
+            });
+        }
+    }
+}
--- DrivingLicense.WebApi/Services/Interfaces/IClientService.cs
+++ DrivingLicense.WebApi/Services/Interfaces/IClientService.cs
@@ -1,7 +1,9 @@
+using DrivingLicense.WebApi.Models;
+
 namespace DrivingLicense.WebApi.Services.Interfaces
 {
     public interface IClientService
     {
-         
+         void AddTicket(LicenseCategory licenseCategory);
     }
 }
